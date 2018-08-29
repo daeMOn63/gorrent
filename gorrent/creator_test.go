fcee70ffe182caaa252cfe24392f633a56415e4e
Flavien Binet <flavien.binet@gmail.com>
2018-08-29 20:51:59 +0200

    Completed gorrent creator tests

--- gorrent/creator_test.go
+++ gorrent/creator_test.go
@@ -1,10 +1,15 @@
 package gorrent
 
 import (
+	"bytes"
+	"compress/gzip"
 	"crypto/sha1"
+	"encoding/gob"
+	"errors"
 	"gorrent/fs"
 	"io"
 	"path/filepath"
+	"reflect"
 	"testing"
 	"time"
 )
@@ -44,11 +49,24 @@ func (f *DummyFile) Size() int64 {
 }
 
 func (f *DummyFile) Read(p []byte) (int, error) {
-	return f.ReadFunc(p)
+	if f.CurReadPtr >= len(f.Content) {
+		return 0, io.EOF
+	}
+
+	maxRead := len(p)
+	if maxRead > len(f.Content[f.CurReadPtr:]) {
+		maxRead = len(f.Content[f.CurReadPtr:])
+	}
+
+	len := maxRead - f.CurReadPtr
+	copy(p, f.Content[f.CurReadPtr:maxRead])
+	f.CurReadPtr += len
+	return len, nil
 }
 
 func (f *DummyFile) Write(p []byte) (int, error) {
-	return f.WriteFunc(p)
+	f.Content = append(f.Content, p...)
+	return len(p), nil
 }
 
 func (f *DummyFile) Close() error {
@@ -60,45 +78,25 @@ func TestCreator(t *testing.T) {
 	pieceBuffer := NewMemoryPieceBuffer(expectedPieceLength)
 
 	expectedFiles := []string{"fileA", "child/fileB", "kittens.jpg"}
-	expectedPath := "some-dir"
+	expectedSourcePath := "some-dir/source"
 	expectedMaxWorkers := 5
 	expectedFileContent := []byte("aaaaaaaaaa")
+	expectedTargetFile := "target/file.gorrent"
 
 	filesystem := &DummyFS{}
 
-	filesystem.OpenFunc = func(path string) (fs.File, error) {
+	properOpenFunc := func(path string) (fs.File, error) {
 		d := &DummyFile{
 			PathVal: path,
 			Content: expectedFileContent,
 		}
 
-		d.ReadFunc = func(buf []byte) (int, error) {
-			if d.CurReadPtr >= len(d.Content) {
-				return 0, io.EOF
-			}
-
-			maxRead := len(buf)
-			if maxRead > len(d.Content[d.CurReadPtr:]) {
-				maxRead = len(d.Content[d.CurReadPtr:])
-			}
-
-			len := maxRead - d.CurReadPtr
-			copy(buf, d.Content[d.CurReadPtr:maxRead])
-			d.CurReadPtr += len
-			return len, nil
-		}
-
-		d.WriteFunc = func(buf []byte) (int, error) {
-			d.Content = append(d.Content, buf...)
-			return len(buf), nil
-		}
-
 		return d, nil
 	}
 
-	filesystem.FindFilesFunc = func(path string, maxWorkers int) ([]string, error) {
-		if path != expectedPath {
-			t.Fatalf("Expected path to be %s, got %s", expectedPath, path)
+	properFindFilesFunc := func(path string, maxWorkers int) ([]string, error) {
+		if path != expectedSourcePath {
+			t.Fatalf("Expected path to be %s, got %s", expectedSourcePath, path)
 		}
 		if maxWorkers != expectedMaxWorkers {
 			t.Fatalf("Expected maxWorkers to be %d, got %d", expectedMaxWorkers, maxWorkers)
@@ -107,9 +105,12 @@ func TestCreator(t *testing.T) {
 	}
 
 	t.Run("Create returns a valid gorrent", func(t *testing.T) {
+		filesystem.FindFilesFunc = properFindFilesFunc
+		filesystem.OpenFunc = properOpenFunc
+
 		creator := NewCreator(pieceBuffer, filesystem)
 
-		g, err := creator.Create(expectedPath, expectedMaxWorkers)
+		g, err := creator.Create(expectedSourcePath, expectedMaxWorkers)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -119,7 +120,7 @@ func TestCreator(t *testing.T) {
 		}
 
 		for i, f := range g.Files {
-			expectedFile := filepath.Join(expectedPath, expectedFiles[i])
+			expectedFile := filepath.Join(expectedSourcePath, expectedFiles[i])
 			if f.Name != expectedFile {
 				t.Fatalf("Expected filename to be %s, got %s", expectedFile, f.Name)
 			}
@@ -145,6 +146,144 @@ func TestCreator(t *testing.T) {
 
 		if len(g.Pieces) != 8 {
 			t.Fatalf("Expected 8 pieces, got %d", len(g.Pieces))
+		}
+	})
+
+	t.Run("Create should handle FindFiles errors properly", func(t *testing.T) {
+		expectedErr := errors.New("findfiles errstring")
+		filesystem.FindFilesFunc = func(path string, maxWorkers int) ([]string, error) {
+			return nil, expectedErr
+		}
+
+		creator := NewCreator(pieceBuffer, filesystem)
+		g, err := creator.Create(expectedSourcePath, expectedMaxWorkers)
+		if g != nil {
+			t.Fatalf("Expected gorrent to be nil")
+		}
+
+		if err != expectedErr {
+			t.Fatalf("Expected error to be %s, got %s", expectedErr, err)
+		}
+	})
+
+	t.Run("Create should handle Open errors properly", func(t *testing.T) {
+		expectedErr := errors.New("open errstring")
+		filesystem.FindFilesFunc = properFindFilesFunc
+		filesystem.OpenFunc = func(path string) (fs.File, error) {
+			return nil, expectedErr
+		}
+
+		creator := NewCreator(pieceBuffer, filesystem)
+		g, err := creator.Create(expectedSourcePath, expectedMaxWorkers)
+		if g != nil {
+			t.Fatalf("Expected gorrent to be nil")
+		}
+
+		if err != expectedErr {
+			t.Fatalf("Expected error to be %v, got %v", expectedErr, err)
+		}
+	})
+
+	t.Run("Save should properly save gorrent", func(t *testing.T) {
+
+		g := &Gorrent{}
+
+		expectedBytes := bytes.NewBuffer(nil)
+
+		gzwriter := gzip.NewWriter(expectedBytes)
+		gob.Register(Gorrent{})
+		encoder := gob.NewEncoder(gzwriter)
+		encoder.Encode(g)
+		gzwriter.Close()
+
+		d := &DummyFile{}
+
+		filesystem.CreateFunc = func(path string) (fs.File, error) {
+			if path != expectedTargetFile {
+				t.Fatalf("Expected path to be %s, got %s", expectedTargetFile, path)
+			}
+
+			d.PathVal = path
+
+			return d, nil
+		}
+
+		creator := NewCreator(pieceBuffer, filesystem)
+
+		err := creator.Save(expectedTargetFile, g)
+		if err != nil {
+			t.Fatalf("Expected no errors, got %s", err)
+		}
+
+		if reflect.DeepEqual(d.Content, expectedBytes.Bytes()) == false {
+			t.Fatalf("Expected file content to be %v, got %v", expectedBytes.Bytes(), d.Content)
+		}
+	})
+
+	t.Run("Save should handle filesystem errors", func(t *testing.T) {
+		expectedErr := errors.New("create errstring")
+		filesystem.CreateFunc = func(path string) (fs.File, error) {
+			return nil, expectedErr
+		}
+
+		g := &Gorrent{}
+
+		creator := NewCreator(pieceBuffer, filesystem)
+		err := creator.Save(expectedTargetFile, g)
+
+		if err != expectedErr {
+			t.Fatalf("Expected error %s, got %s", expectedErr, err)
+		}
+	})
+
+	t.Run("Open should properly open gorrent", func(t *testing.T) {
+
+		expectedGorrent := &Gorrent{}
+
+		expectedBytes := bytes.NewBuffer(nil)
+
+		gzwriter := gzip.NewWriter(expectedBytes)
+		gob.Register(Gorrent{})
+		encoder := gob.NewEncoder(gzwriter)
+		encoder.Encode(expectedGorrent)
+		gzwriter.Close()
+
+		d := &DummyFile{}
+
+		filesystem.OpenFunc = func(path string) (fs.File, error) {
+			if path != expectedTargetFile {
+				t.Fatalf("Expected path to be %s, got %s", expectedTargetFile, path)
+			}
+
+			d.PathVal = path
+			d.Content = expectedBytes.Bytes()
+
+			return d, nil
+		}
+
+		creator := NewCreator(pieceBuffer, filesystem)
+
+		g, err := creator.Open(expectedTargetFile)
+		if err != nil {
+			t.Fatalf("Expected no errors, got %s", err)
+		}
+
+		if reflect.DeepEqual(g, expectedGorrent) == false {
+			t.Fatalf("Expected gorrent to be %v, got %v", expectedGorrent, g)
+		}
+	})
+
+	t.Run("Open should properly handle filesystem errors", func(t *testing.T) {
+		expectedErr := errors.New("open errstring")
+		filesystem.OpenFunc = func(path string) (fs.File, error) {
+			return nil, expectedErr
+		}
+
+		creator := NewCreator(pieceBuffer, filesystem)
+		_, err := creator.Open(expectedTargetFile)
+
+		if err != expectedErr {
+			t.Fatalf("Expected error %s, got %s", expectedErr, err)
 		}
 	})
 
